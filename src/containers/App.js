528882af01f0f54036c8d29f84a62bb9500f97f8
林玉划 <linyuhua@linyuhuadeMacBook-Pro.local>
2017-03-06 20:36:40 +0800

    Add container

---
+++ src/containers/App.js
@@ -0,0 +1,16 @@
+/**
+ * Created by linyuhua on 2017/3/6.
+ */
+import React,{Component} from 'react';
+
+class App extends Component {
+    render() {
+        return(
+            <div>
+                <h1>react-async</h1>
+            </div>
+        )
+    }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+/**
+ * Created by linyuhua on 2017/3/6.
+ */
+import React from 'react';
+import {render} from 'react-dom';
+
+import App from './containers/App';
+
+render(
+    <App />,
+    document.getElementById('root')
+)
